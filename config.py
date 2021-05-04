564e80037ce16725ff7f77e8dfb408e5ac25967d
karnabou <82140858+karnabou@users.noreply.github.com>
2021-05-04 15:46:42 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # Importer le modèle os
 import os
 # Récupérer les variables d'environnement liées à S3
-S3_BUCKET = os.environ.get("hichambucket")
-S3_KEY = os.environ.get("AKIA5VW6Q22K3N7IEQFK'")
-S3_SECRET = os.environ.get("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")
+S3_BUCKET = os.environ.get("S3BUCKET")
+S3_KEY = os.environ.get("S3_KEY")
+S3_SECRET = os.environ.get("S3_SECRET")
