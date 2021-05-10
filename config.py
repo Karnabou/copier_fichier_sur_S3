818789d61c128fa425cd127a36e4d0fde5cc29dd
karnabou <82140858+karnabou@users.noreply.github.com>
2021-05-10 20:05:30 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
 # Importer le modèle os
+#  Identification sur Bucket S3 - version 1.0
+# Importer le modèle os
 import os
 # Récupérer les variables d'environnement liées à S3
 S3_BUCKET = os.environ.get("S3_BUCKET")
