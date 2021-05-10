6af9aa315be5cda6ac4f085a2d29d6d2f43227ea
karnabou <82140858+karnabou@users.noreply.github.com>
2021-05-10 20:04:17 +0200

    Update filters.py

--- filters.py
+++ filters.py
@@ -1,4 +1,5 @@
-# Importer les modules pour obtenir le type de fichier
+#  Filtrer les éléments - version 1.0
+#  Importer les modules pour obtenir le type de fichier
 
 import os
 import mimetypes
@@ -6,12 +7,11 @@ import mimetypes
 # Importer la bibliotèque datetime
 import arrow
 
-
 additional_file_types = {
     '.md': 'text/markdown'
 }
 
-# Focntion date 
+# Focntion date
 def datetimeformat(date_str):
     dt = arrow.get(date_str)
     return dt.humanize(locale='fr')
